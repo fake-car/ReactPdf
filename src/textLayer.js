5548622e3ecca283ed4206482d5f3b039ea1cf57
inter <interlgy@hotmail.com>
2019-02-22 11:10:07 +0800

    create text-layer

---
+++ src/textLayer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class textLayer extends Component {
+  render() {
+    return (
+      <div>
+        {
+          // TODO separate textLayer from canvas
+        }
+      </div>
+    )
+  }
+}
+
+export default textLayer
